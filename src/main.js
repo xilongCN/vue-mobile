7323ec303242846b5949ed39db4029b580d812fa
Work-PC <254282072@qq.com>
2018-09-06 14:39:05 +0800

    css结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/router'
+import store from './vuex/store'
+import FastClick from 'fastclick'
 
 Vue.config.productionTip = false
-
+FastClick.attach(document.body)
 new Vue({
   router,
   store,
