42040f025c86e9467dbfa43aa798481bbf9c2a04
Work-PC <254282072@qq.com>
2018-09-18 14:48:04 +0800

    px2rem

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,18 @@
 // import px2rem from 'postcss-plugin-px2rem';
-let px2rem = require('postcss-plugin-px2rem')
+// let px2rem = require('postcss-plugin-px2rem')
 module.exports = {
   lintOnSave: false,
-  configureWebpack: {
-    // module: {
-    //     loaders: [
-    //         {
-    //             test: /\.css$/,
-    //             loader: 'style-loader!css-loader!postcss-loader',
-    //         },
-    //     ],
-    // },
-    postcss: [px2rem({ rootValue: 75 })]
+  css: {
+    loaderOptions: {
+      postcss: {
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          plugins: (loader) => [
+            require('postcss-plugin-px2rem')({ rootValue: 75 })
+          ]
+        }
+      }
+    }
   }
 }
